6edd9027e22f26f3abda6036366f8a3574b17092
Mamidi Shyam Manideep <shyammanideep.m@engro.io>
2021-08-10 17:41:37 +0530

    learn

--- app.js
+++ app.js
@@ -1,14 +1,58 @@
 // console.log('Hello Node')
 // console.log('Process', process)
 
-const {sum}= require("./healper")
+// const {sum}= require("./healper")
 
-const total =sum(3, 9)
-console.log("Total sum value", total)
+// const total =sum(3, 9)
+// console.log("Total sum value", total)
 
-const http = require("http")
+// const http = require("http")
 
-const server = http.createServer((req, res)=> {
-    res.end('hello world from node.js ')
+// const server = http.createServer((req, res)=> {
+//     res.end('hello world from node.js ')
+// } )
+// server.listen(3000)
+
+// const express = require('express');
+
+// const app = express();
+// app.get('/', (req, res) => {
+//     res.send('Hey this is from express.js')
+// } )
+// app.listen(3000)
+
+// Event looping---------------------------------------
+// const fs = require('fs')
+// const fileName = 'file.txt';
+// fs.watch(fileName, ()=> console.log('file changed now....!') )
+
+// const fs =require('fs')
+// const fileName = 'file.txt';
+// fs.readFile(fileName, (err, data) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString())
+// } )
+// console.log('Hello this is Asynchronous Programming....!')
+
+// const fs = require('fs');
+// const fileName = "file.txt";
+
+// const data = fs.readFileSync(fileName)
+// console.log(data.toString())
+// console.log('Hello this is an synchronous Programming....')
+
+const fs = require ('fs')
+const fileName = 'file.txt';
+const errHandler = err => console.log(err);
+const dataHandler = data => console.log(data.toString())
+fs.readFile(fileName, (err, data) => {
+    if(err){
+        errHandler(err)
+    }
+    dataHandler(data)
+
 } )
-server.listen(3000)
+
+console.log('nOde js functional approach')
